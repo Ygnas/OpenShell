fadf4edabcd4ca80d566ef4a9e4e9ff910cbb758
Varsha Prasad Narsing <varshaprasad96@gmail.com>
2026-09-14 19:27:08 -0700

    refactor(crypto): centralize AWS-LC behind backend interface
    Signed-off-by: Varsha Prasad Narsing <varshaprasad96@gmail.com>

--- crates/openshell-server/src/provider_refresh.rs
+++ crates/openshell-server/src/provider_refresh.rs
@@ -1614,7 +1614,7 @@ async fn request_token(
         }
     }
 
-    let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
+    openshell_crypto::tls::ensure_default_provider();
     let client = reqwest::Client::builder()
         .timeout(Duration::from_secs(30))
         .build()
@@ -2536,7 +2536,7 @@ mod tests {
             .mount(&mock_server)
             .await;
 
-        let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
+        openshell_crypto::tls::ensure_default_provider();
         let response = reqwest::get(format!("{}/oversized", mock_server.uri()))
             .await
             .unwrap();
